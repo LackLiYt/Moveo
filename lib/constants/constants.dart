7b2a6e5b2f75e0dec843a61f1f67104915cfd7b0
LackLiYt <41758945+LackLiYt@users.noreply.github.com>
2024-11-16 19:27:37 +0200

    Працююча аутентифікація і мінус сміття

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,5 +1,5 @@
-const String APPWRITE_PROJECT_ID = "643cfa90ef6cdc96a2a9";
+const String APPWRITE_PROJECT_ID = "6717a8f8000e772c5899";
 const String APPWRITE_URL = "https://cloud.appwrite.io/v1";
 
-const String APPWRITE_DATABASE_ID = "643d04665fd8051f19be";
-const String COLLECTION_MESSAGES = "643d047649b1cf70e36e";
+const String APPWRITE_DATABASE_ID = "6717aac9002fc50506d7";
+const String COLLECTION_MESSAGES = "673796160022761894a5";
